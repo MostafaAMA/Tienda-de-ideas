40a75efb3a83723cd889a82960d2ab542edb6832
MostafaAMA <79628911+MostafaAMA@users.noreply.github.com>
2021-12-15 15:08:47 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,60 @@
-var sections = document.querySelectorAll("section");
-var h2 = document.querySelectorAll("h2");
 
-h2.addEventListener('click', () => {
-    if (sections[i].style.display === 'none'){
-        sections[i].style.display = 'block';
+// show & hide vars
+var Dev1 = document.querySelector(".hd1");
+var h1 = document.querySelector(".dv1");
+var Dev2 = document.querySelector(".hd2");
+var h2 = document.querySelector(".dv2");
+var Dev3 = document.querySelector(".hd3");
+var h3 = document.querySelector(".dv3");
+var Dev4 = document.querySelector(".hd4");
+var h4 = document.querySelector(".dv4");
+var Dev5 = document.querySelector(".hd5");
+var h5 = document.querySelector(".dv5");
+var Dev6 = document.querySelector(".hd6");
+var h6 = document.querySelector(".dv6");
+
+// show & hide function
+Dev1.addEventListener('click', function() {
+    if (h1.style.display === 'none'){
+        h1.style.display = 'block';
     }else {
-        sections[i].style.display = 'none';
+        h1.style.display = 'none';
     }
 })
+Dev2.addEventListener('click', function() {
+    if (h2.style.display === 'none'){
+        h2.style.display = 'block';
+    }else {
+        h2.style.display = 'none';
+    }
+})
+Dev3.addEventListener('click', function() {
+    if (h3.style.display === 'none'){
+        h3.style.display = 'block';
+    }else {
+        h3.style.display = 'none';
+    }
+})
+Dev4.addEventListener('click', function() {
+    if (h4.style.display === 'none'){
+        h4.style.display = 'block';
+    }else {
+        h4.style.display = 'none';
+    }
+})
+Dev5.addEventListener('click', function() {
+    if (h5.style.display === 'none'){
+        h5.style.display = 'block';
+    }else {
+        h5.style.display = 'none';
+    }
+})
+Dev6.addEventListener('click', function() {
+    if (h6.style.display === 'none'){
+        h6.style.display = 'block';
+    }else {
+        h6.style.display = 'none';
+    }
+})
+
+
